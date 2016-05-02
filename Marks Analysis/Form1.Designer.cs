12ff716465a922b3ea4c86a5592402ffa4e5faa2
mitccase <mitccase854@ed.amdsb.ca>
2016-05-02 09:31:35 -0400

    Almost finished marks analysis program. All necessary elements are included. Works properly.

---
+++ Marks Analysis/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Marks_Analysis
+{
+    partial class marksAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.marksBox = new System.Windows.Forms.TextBox();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(291, 62);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 24);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // marksBox
+            // 
+            this.marksBox.Location = new System.Drawing.Point(168, 66);
+            this.marksBox.Name = "marksBox";
+            this.marksBox.Size = new System.Drawing.Size(70, 20);
+            this.marksBox.TabIndex = 1;
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.analyzeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.analyzeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.analyzeButton.Location = new System.Drawing.Point(179, 426);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(75, 24);
+            this.analyzeButton.TabIndex = 2;
+            this.analyzeButton.Text = "Analyze";
+            this.analyzeButton.UseVisualStyleBackColor = false;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 4;
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(190, 471);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(48, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 294);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(273, 116);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(182, 294);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(113, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 32);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Marks Analysis";
+            // 
+            // marksAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(485, 506);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.analyzeButton);
+            this.Controls.Add(this.marksBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "marksAnalysis";
+            this.Text = "marksAnalysis";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox marksBox;
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Marks Analysis/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marks_Analysis
+{
+    public partial class marksAnalysis : Form
+    {
+        public marksAnalysis()
+        {
+            InitializeComponent();
+        }
+        bool canClick = false;
+        int n;
+        List<int> grades = new List<int>() { };
+        private void addButton_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                label1.Text = "";
+                n=(int.Parse(marksBox.Text));//changes text into integer
+                if (n >= 0 && n <=100)//numbers has to be in between 0-100
+                {
+                    canClick = true;//analyze button can be clicked
+                    grades.Add(n);//adds it to list
+                    label3.Text = "";
+                }
+                else { label3.Text = "Must enter number between 0-100"; }//if number is not in between 0-100
+
+            }
+            catch (FormatException)
+            {
+                label3.Text = "You must enter an integer";//if text is not a number
+            }
+            catch (IndexOutOfRangeException)
+            {
+                label3.Text = "Array is full";
+            }
+            for (int i = 0; i < grades.Count(); i++)
+            {
+                grades.Sort();//sorts list numbers in order
+                label1.Text += "\n" + grades[i] + "%";//organizes the marks and adds percent
+            }
+            marksBox.Clear();
+        }
+
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            if (canClick == true)
+            {
+                label3.Text = "";
+                int avg = 0;
+                int avg1 = 0;
+                int levelR = 0;
+                int level1 = 0;
+                int level2 = 0;
+                int level3 = 0;
+                int level4 = 0;
+                int minimum = 0;
+                int maximum = 0;
+                for (int i = 0; i < grades.Count(); i++)
+                {
+                    avg1 += grades[i];//adds all numbers in the list
+                    if (minimum == 0)
+                    {
+                        //minimum and maximum is equal to first grade inputed
+                        maximum = grades[i];
+                        minimum = grades[i];
+                    }
+                    if (minimum > 0 || maximum > 0)
+                    {
+                        //if mimimum is greater than new grade inputed then it changes to that number
+                        if (minimum > grades[i])
+                        {
+                            minimum = grades[i];
+                        }
+                        //if maximum is less than new grade inputed then it changes to that number
+                        if (maximum < grades[i])
+                        {
+                            maximum = grades[i];
+                        }
+                    }
+                    if (grades[i] < 50)//if less than 50
+                    {
+                        levelR += 1;
+                    }
+                    if (grades[i] >= 50 && grades[i] < 60)//if more than 50 but less than 60
+                    {
+                        level1 += 1;
+                    }
+                    if (grades[i] >= 60 && grades[i] < 70)//if more than 60 but less than 70
+                    {
+                        level2 += 1;
+                    }
+                    if (grades[i] >= 70 && grades[i] < 80)//if more than 70 but less than 80
+                    {
+                        level3 += 1;
+                    }
+                    if (grades[i] >= 80)//if more than 80
+                    {
+                        level4 += 1;
+                    }
+                }
+                avg = avg1 / grades.Count();//divides sum of all list numbers by total numbers in list
+                //writes all the values wanted
+                label2.Text = "\nClass Average:     " + avg + "%";
+                label2.Text += "\nMinimum Mark:    " + minimum + "%";
+                label2.Text += "\nMaximum Mark:   " + maximum + "%";
+                label2.Text += "\nRange of Marks:   " + (maximum - minimum);
+                label2.Text += "\nNumber at Level R: " + levelR;
+                label2.Text += "\nNumber at Level 1: " + level1;
+                label2.Text += "\nNumber at Level 2: " + level2;
+                label2.Text += "\nNumber at Level 3: " + level3;
+                label2.Text += "\nNumber at Level 4: " + level4;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            canClick = false;//analyze button can no longer be clicked
+            label1.Text = "";
+            label2.Text = "";
+            grades.Clear();//clears all numbers in list
+        }
+    }
+}
+ 
